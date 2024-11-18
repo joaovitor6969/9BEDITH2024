3180266d690715018e8e8544e7e5b5fcc4151e03
joaovitor6969 <joao.corol@escola.pr.gov.br>
2024-11-18 11:13:54 -0300

    aula 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function setup () {
+    createcancas(400,400);
+    let palavra = promot ("qual palavra deseja   codificar");
+
+    background("#01080e");
+    console.log(palavra);
+
+    noloop();
+}
